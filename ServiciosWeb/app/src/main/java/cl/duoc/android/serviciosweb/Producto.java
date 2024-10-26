46922533fa260954126e153bd31f2de9dd662d97
Marcelo Burgos <marchelo.bm@gmail.com>
2024-10-26 04:24:00 -0300

    v1.0 Inicio
    Recuperacion del Proyecto

---
+++ ServiciosWeb/app/src/main/java/cl/duoc/android/serviciosweb/Producto.java
@@ -0,0 +1,34 @@
+package cl.duoc.android.serviciosweb;
+
+public class Producto {
+
+    private String codigo;
+    private String nombre;
+
+    public Producto() {
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
