eeb1772c8100bc1c1fd56c7fd82af87677917302
Marcelo Burgos <marchelo.bm@gmail.com>
2025-10-26 02:46:48 -0300

    v1.4.2 Cambios
    cambios para el APK

--- ServiciosWeb/app/src/androidTest/java/cl/duoc/android/serviciosweb/ApplicationTest.java
+++ ServiciosWeb/app/src/androidTest/java/cl/duoc/android/serviciosweb/ApplicationTest.java
@@ -1,13 +1,23 @@
 package cl.duoc.android.serviciosweb;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@org.junit.runner.RunWith(androidx.test.ext.junit.runners.AndroidJUnit4.class)
+public class ApplicationTest {
+    @org.junit.Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().getTargetContext();
+        org.junit.Assert.assertEquals("cl.duoc.android.serviciosweb", appContext.getPackageName());
     }
 }
